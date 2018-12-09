36997abe6be3e01785e43fe41158cba4c35cf32b
Iker Suen <suenik@oocl.com>
2018-12-09 16:22:55 +0800

    GET Order requires authorization

--- src/main/java/com/binluis/parkingsystem/controller/OrderResource.java
+++ src/main/java/com/binluis/parkingsystem/controller/OrderResource.java
@@ -8,6 +8,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
@@ -31,6 +32,7 @@ public class OrderResource {
     ParkingLotRepository parkingLotRepository;
 
     @GetMapping(produces = {"application/json"})
+    @PreAuthorize("hasRole('ROLE_USER')")
     public ResponseEntity<List<ParkingOrder>> showAllOrders() {
         List<ParkingOrder> allOrders = parkingOrderRepository.findAll();
         return ResponseEntity.ok(allOrders);
